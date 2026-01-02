70e6fa3269069dfb9e2e77afea4c16aec61c7301
neelam1424 <neelammore1404@gmail.com>
2026-01-02 17:44:44 -0500

    Building Authentication logic and prisma schema

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,44 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
-// import {
-//   DEFAULT_LOGIN_REDIRECT,
-//   apiAuthPrefix,
-//   publicRoutes,
-//   authRoutes,
-// } from "@/routes";
-// import authConfig from "./auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from "@/routes";
+import authConfig from "./auth.config";
 
-// const { auth } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
 
-// export default auth((req) => {
-//   const { nextUrl } = req;
-//   const isLoggedIn = !!req.auth;
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
-//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
-//   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-//   if (isApiAuthRoute) {
-//     return null;
-//   }
+  if (isApiAuthRoute) {
+    return null;
+  }
 
-//   if (isAuthRoute) {
-//     if (isLoggedIn) {
-//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-//     }
-//     return null;
-//   }
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
 
-//   if (!isLoggedIn && !isPublicRoute) {
-//     return Response.redirect(new URL("/auth/sign-in", nextUrl));
-//   }
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/sign-in", nextUrl));
+  }
 
-//   return null;
-// });
+  return null;
+});
 
-// export const config = {
-//   // copied from clerk
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+export const config = {
+  // copied from clerk
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
